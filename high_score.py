6f4965abd1b8ad312e041fe373046ca9a42e1525
Denick <1049485+apie@users.noreply.github.com>
2019-11-02 16:26:57 +0100

    Dubbele code weghalen uit high_score.py
    Fixes #13

--- high_score.py
+++ high_score.py
@@ -39,43 +39,35 @@ def set_high_score(first_name, user_id, rounds, duration):
 	      date=datetime.datetime.now())
     db.commit()
 
-def get_high_scores(user_id):
+def get_high_scores(user_id=None):
     print('get_high_scores')
     db = Base(os.path.join(SCRIPT_DIR, 'high_scores.db'))
     db.create('user_id', 'score', 'duration', 'date', mode="open")
-    my_top = []
-    for r in sorted([r for r in db if r['user_id'] == user_id ], key=sort_high_scores):
-        my_top.append({
+    global_top = [
+        {
+            'user_id': r['user_id'],
+            'user': get_user_name(r['user_id']),
             'date': r['date'].strftime('%d-%m-%Y'),
             'duration': str(r['duration']).split('.')[0],
             'score': r['score'],
-        })
-    global_top = []
-    for r in sorted([ r for r in db ], key=sort_high_scores):
-        global_top.append({
-            'user': get_user_name(r['user_id']),
-            'duration': str(r['duration']).split('.')[0],
-            'score': r['score'],
-        })
+        }
+        for r in sorted([ r for r in db ], key=sort_high_scores)
+    ]
     return {
-    	'my_top': my_top[:10],
-      'global_top': global_top[:10],
+        'my_top': [r for r in global_top if r['user_id'] == user_id][:5],
+        'global_top': global_top[:5],
     }
 
 def get_print_high_scores():
-    db = Base(os.path.join(SCRIPT_DIR, 'high_scores.db'))
-    db.create('user_id', 'score', 'duration', 'date', mode="open")
-    scores = []
-    for r in sorted([ r for r in db ], key=sort_high_scores):
-      scores.append('{user} {score:02} {duration} {date}'.format(
-        user=get_user_name(r['user_id']),
-        score=r['score'],
-        date=r['date'].strftime('%d-%m-%Y'),
-        duration=str(r['duration']).split('.')[0],
-      ))
-      if len(scores) == 10:
-        break
-    return scores
+    return [
+        '{user} {score:02} {duration} {date}'.format(
+            user=r['user'],
+            score=r['score'],
+            date=r['date'].strftime('%d-%m-%Y'),
+            duration=str(r['duration']).split('.')[0],
+        )
+        for r in get_high_scores()['global_top']
+    ]
 
 def print_high_scores():
     for score in get_print_high_scores():
